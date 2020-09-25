f861d40e6dd4fc627a2a9f18fbc34994fa8f71df
Chance Smith <chancesmith97@hotmail.com>
2020-09-25 23:21:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 let num1 = 62
 let num2 = 1
 let multiply = num1 * num2
-multiply
 
 
 let random = Math.floor(Math.random() * 10) + 1
-random
 
 let mod = (20 % 8)
-mod
 
 let max = Math.max(5, 10, 15, 20)
